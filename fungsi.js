a2b5b4f78342baa69c42101dabdc8d451c15f218
Snakesystem <90812378+feri-irawansyah@users.noreply.github.com>
2025-07-13 14:18:03 +0700

    Tambah warna warni

--- fungsi.js
+++ fungsi.js
@@ -2,3 +2,11 @@ console.log ('fungsi pertama');
 
 const add = (a, b) => a + b;
 console.log (add(1,2));
+
+function tombolHijau () {
+    alert("Tombol hijau di pencet")
+}
+
+function tombolMerah () {
+    alert("Tombol merah di pencet")
+}
